8fe2ff356d46e83ee6a36b620396b14a120665a6
Ronny Müller <ronny.mueller@ost.ch>
2022-11-02 16:51:57 +0100

    Draft for additional binaries with different compilers

---
+++ RE_Attempts/binary/code/tinyc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("running Program \n");
+    if(argc == 2 && strcmp(argv[1], "aH6Gn4I") == 0) {
+        printf("Success! The Flag is: arguments");
+    } else {
+        printf("requirements not met");
+    }
+}
