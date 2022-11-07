7921a2c0fc8df9b6d0cba5fe674ea6d69dc5393f
Ronny Müller <ronny.mueller@ost.ch>
2022-11-07 18:40:30 +0100

    draft new files

--- RE_Attempts/binary/code/tinyc.c
+++ RE_Attempts/binary/code/tinyc.c
@@ -3,7 +3,8 @@
 int main(int argc, char *argv[]) {
     printf("running Program \n");
     if(argc == 2 && strcmp(argv[1], "aH6Gn4I") == 0) {
-        printf("Success! The Flag is: arguments");
+        char[] flag = "someStuff";
+        printf("Success! The Flag is: %s", &flag);
     } else {
         printf("requirements not met");
     }
