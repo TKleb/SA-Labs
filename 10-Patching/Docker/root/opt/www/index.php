1d067919a81bd5f73055784910c1b89f93654293
Gianluca Nenz <gianluca.nenz@ost.ch>
2022-12-05 16:12:35 +0100

    Update title of website

--- 10-Patching/Docker/root/opt/www/index.php
+++ 10-Patching/Docker/root/opt/www/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>RE Labs - Remote Login</title>
+    <title>RE Labs - Patching</title>
     <link rel="stylesheet" type="text/css" href="main.css" />
   </head>
   <body>
